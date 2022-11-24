be35424ec6f9492876e0fc6ca1de3e787e51eaaf
Grazielle Moura <mouragrazielle@gmail.com>
2022-11-24 17:53:57 -0300

    identar codigo

--- src/pages/Portifolio.jsx
+++ src/pages/Portifolio.jsx
@@ -4,19 +4,19 @@ export function Portifolio() {
         <div>
             <h1>Conheça um pouco meus trabalhos</h1>
 
-                <div>
-                    <a href="https://www.linkedin.com/in/grazielle-moura-777830179/" target="_blank">
-                        <img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" />
-                    </a>
+            <div>
+                <a href="https://www.linkedin.com/in/grazielle-moura-777830179/" target="_blank">
+                    <img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" />
+                </a>
 
-                    <a href="https://github.com/mouragra" target="_blank">
-                        <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" />
-                    </a>
+                <a href="https://github.com/mouragra" target="_blank">
+                    <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" />
+                </a>
 
-                    <a href="" target="_blank">
-                        <img src="" />
-                    </a>
-                </div>
+                <a href="" target="_blank">
+                    <img src="" />
+                </a>
+            </div>
         </div>
     )
 }
